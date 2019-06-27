c92c75eff7bccbe5495652a2a3653c6a490ffe0f
Archanayadav136 <Archanayadav136@gmail.com>
2019-06-27 14:17:06 +0530

    Day 3 commit 4 actuator

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -1,22 +1,16 @@
 package io.pivotal.pal.tracker;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class TimeEntry {
     private long id;
+    private long projectId;
     private long userId;
     private LocalDate date;
     private int hours;
-    private long projectId;
 
-    public TimeEntry(long id) {
-        this.id = id;
-    }
     public TimeEntry() {
-
     }
-
 
     public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
         this.projectId = projectId;
@@ -45,43 +39,18 @@ public class TimeEntry {
         return projectId;
     }
 
-    public void setProjectId(long projectId) {
-        this.projectId = projectId;
-    }
-
     public long getUserId() {
         return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public int getHours() {
         return hours;
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-    @Override
-    public String toString() {
-        return "TimeEntry{" +
-                "id=" + id +
-                ", projectId=" + projectId +
-                ", userId=" + userId +
-                ", date=" + date +
-                ", hours=" + hours +
-                '}';
-    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -104,5 +73,16 @@ public class TimeEntry {
         result = 31 * result + (date != null ? date.hashCode() : 0);
         result = 31 * result + hours;
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeEntry{" +
+            "id=" + id +
+            ", projectId=" + projectId +
+            ", userId=" + userId +
+            ", date='" + date + '\'' +
+            ", hours=" + hours +
+            '}';
     }
 }
